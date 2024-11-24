cbf53c6b14fd9be00c6899276f6ed9a929002b68
Harascho1 <luka.old1@gmail.com>
2024-11-24 22:22:44 +0100

    ponovo ja

--- lab2/tmp/batchovic.bat
+++
@@ -1,6 +0,0 @@
-ls -al  
-touch novifajl.txt
-ls -al  
-cp zadatak1.c novifajl.txt
-cat novifajl.txt
-rm novifajl.txt
--- lab4/nikoli.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-	for(int i = 0; i < 100; i++) {
-		//pisu na standardni izlaz
-		printf("JA BRATE\n");
-	}
-	return 0;
-}
